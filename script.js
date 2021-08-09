033c9721a6c525e8535f4d29bea6084e966df74f
Ravi Kumar <69725593+Ravikr0602@users.noreply.github.com>
2021-08-09 14:59:12 +0530

    display digital clock time by using Java Script

---
+++ script.js
@@ -0,0 +1,29 @@
+function displayTime(){
+    var date = new Date();
+    var hour = date.getHours(); // 0 - 23
+    var minute = date.getMinutes(); // 0 - 59
+    var second = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(hour == 0){
+        hour = 12;
+    }
+    
+    if(hour > 12){
+        hour = hour - 12;
+        session = "PM";
+    }
+    
+    hour = (hour < 10) ? "0" + hour : hour;
+    minute = (minute < 10) ? "0" + minute : minute;
+    second = (second < 10) ? "0" + second : second;
+    
+    var time = hour + ":" + minute + ":" + second + " " + session;
+    document.getElementById("clock_time").innerText = time;
+    document.getElementById("clock_time").textContent = time;
+    
+    setTimeout(displayTime, 1000);
+    
+}
+
+displayTime();
